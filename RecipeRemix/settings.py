d946d12fe98e3ec08af5ace3255b11d36b5b84b4
Matthew <matthewkaminski15@gmail.com>
2022-02-14 18:05:42 -0500

    added registration functionality

--- RecipeRemix/settings.py
+++ RecipeRemix/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'app.apps.AppConfig',
+	'register.apps.RegisterConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
