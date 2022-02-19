afe19b2862ff03f72d13657de4a72b589bcc0067
Alex Lowe <lowea18@students.ecu.edu>
2022-02-18 20:19:22 -0500

    feat: Allow Users to Create Accounts

--- register/views.py
+++ register/views.py
@@ -1,15 +1,25 @@
+from django.contrib.auth import get_user_model, login
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
+User = get_user_model()
+
 
 # Create your views here.
-def register(response):
-	if response.method == "POST":
-		form = RegisterForm(response.POST)
-		if form.is_valid():
-			form.save()
-		return redirect("/home")
-	else:
-		form = RegisterForm()
-
-	return render(response, "register/register.html", {"form": form})
+def register(request):
+	form = RegisterForm(request.POST or None)
+	if form.is_valid():
+		username = form.cleaned_data.get("username")
+		email = form.cleaned_data.get("email")
+		password = form.cleaned_data.get("password")
+		password2 = form.cleaned_data.get("password_confirm")
+		try:
+			user = User.objects.create_user(username, email, password)
+		except:
+			user = None
+		if user != None:
+			login(request, user)
+			return redirect("/")
+		else:
+			request.session['register_error'] = 1
+	return render(request, "register/register.html", {"form": form})
