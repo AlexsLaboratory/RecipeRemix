da6c9ba532e9fb2faf8543a3bf8fcb4fd32e482a
Alex Lowe <lowea18@students.ecu.edu>
2022-03-13 16:27:45 -0400

    fix: "Remove empty" Button Not Decrementing TOTAL_FORMS variable
    Ref: #5

--- app/static/js/add-allergy.js
+++ app/static/js/add-allergy.js
@@ -5,19 +5,16 @@ addMoreBtn.addEventListener("click", (e) => {
   const currentAllergyForm = document.querySelectorAll(".allergy-form");
   const formCopyTarget = document.getElementById("allergy-form-list");
   const cloneEmptyFormEl = document.getElementById("empty-form").cloneNode(true);
-  const currentAllergyFormCount = currentAllergyForm.length;
   cloneEmptyFormEl.setAttribute("class", "allergy-form");
   cloneEmptyFormEl.removeAttribute("id");
   const regex = new RegExp("__prefix__", "g");
-  cloneEmptyFormEl.innerHTML = cloneEmptyFormEl.innerHTML.replace(regex, currentAllergyFormCount);
-  totalNewForms.value = `${currentAllergyFormCount + 1}`;
+  cloneEmptyFormEl.innerHTML = cloneEmptyFormEl.innerHTML.replace(regex, totalNewForms.value);
+  totalNewForms.value = `${parseInt(totalNewForms.value) + 1}`
   formCopyTarget.append(cloneEmptyFormEl)
 });
 
 removeEmptyBtn.addEventListener("click", (e) => {
-  const currentAllergyForm = document.querySelectorAll(".allergy-form");
-  const currentAllergyFormCount = currentAllergyForm.length;
-  totalNewForms.setAttribute("value", `${currentAllergyFormCount - 1}`)
+  totalNewForms.value = `${parseInt(totalNewForms.value) - 1}`
   const allergyFormList = document.getElementById("allergy-form-list");
   if (allergyFormList.lastElementChild.querySelector("input[type='text']").value !== "") return;
   allergyFormList.removeChild(allergyFormList.lastChild);
