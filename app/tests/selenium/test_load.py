44a4f64c7e17bf45e36ae1908333938e8ef0441b
Alex Lowe <lowea18@students.ecu.edu>
2022-03-22 14:17:45 -0400

    test: Setup Structure for Selenium and Run Example Test

---
+++ app/tests/selenium/test_load.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+
+class TestTitle:
+	def setup_method(self):
+		self.driver = webdriver.Remote(command_executor='http://selenium-hub:4444/wd/hub',
+									   options=webdriver.ChromeOptions())
+		self.vars = {}
+
+	def teardown_method(self):
+		self.driver.quit()
+
+	def test_title(self):
+		self.driver.get("https://recipe-remix.tech/")
+		self.driver.set_window_size(788, 816)
+		assert self.driver.title == "Recipe Remix"
