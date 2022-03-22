b2260adca3ad77561c5a53ce10b810b5a09535dc
Alex Lowe <lowea18@students.ecu.edu>
2022-03-22 11:57:33 -0400

    test: Update Tests to Use Pytest

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,23 +1,24 @@
+import pytest
 from django.contrib.auth import get_user_model
-from django.test import TestCase
 
 
-class AccountTestCase(TestCase):
-	def setUp(self):
+@pytest.mark.django_db(serialized_rollback=True)
+class TestAccount:
+
+	@pytest.fixture()
+	def setup(self):
 		user_model = get_user_model()
-		self.u1 = user_model.objects.create_user(
+		u1 = user_model.objects.create_user(
 			email="test@recipe-remix.tech",
 			username="test",
 			password="n&M0jU3@S*"
 		)
-		self.u1.is_admin = False
-		self.u1.is_active = True
-		self.u1.is_staff = False
-		self.u1.save()
+		u1.is_admin = False
+		u1.is_active = True
+		u1.is_staff = False
+		u1.save()
+		return u1
 
-	def testAccountExists(self):
+	def test_create_user(self, setup):
 		user_email = get_user_model().objects.get(email="test@recipe-remix.tech").email
-		self.assertEqual(user_email, "test@recipe-remix.tech")
-
-	def tearDown(self):
-		self.u1.delete()
+		assert user_email == "test@recipe-remix.tech"
